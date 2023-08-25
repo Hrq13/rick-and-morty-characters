96f1e26515eb6d245f32b07f0d0f8d9f6f47d887
Henrique F. Miranda <henriqemiranda15@gmail.com>
2023-08-25 01:41:49 -0300

    chore: set default viewport width in cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
+    viewportWidth: 1020,
     devServer: {
-      framework: "vue",
-      bundler: "vite",
-    },
-  },
+      framework: 'vue',
+      bundler: 'vite'
+    }
+  }
 });
