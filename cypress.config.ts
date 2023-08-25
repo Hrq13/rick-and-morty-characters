f2752065340750629cbdd631c7b80086006d79be
Henrique F. Miranda <henriqemiranda15@gmail.com>
2023-08-25 03:23:35 -0300

    chore: disable video and screenshot when cypress fails

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
+    video: false,
+    screenshotOnRunFailure: false,
     viewportWidth: 1020,
     devServer: {
       framework: 'vue',
